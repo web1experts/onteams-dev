e92cad193fe4a280a1b1d621fdd942b0538a86ca
Web 1 Experts <web1experts@Webs-iMac.local>
2026-03-06 18:06:23 +0530

    desgin changes

--- src/Components/subscriptions/success.js
+++ src/Components/subscriptions/success.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { FaRegCheckCircle } from "react-icons/fa";
 import { Container } from "react-bootstrap";
 
 function SuccessPage() {
@@ -7,12 +8,13 @@ function SuccessPage() {
 
   return (
     <>
-        <div className="team--page subscription--page">
+        <div className="team--page subscription--page success--page">
             <div className="page--wrapper px-md-2 pb-4 pt-4 py-5 pt-5 text-center h-100">
                 <Container>
-                    <h2 className="text-center mb-1">Success</h2>
-                    <p className="text-center mb-4">Your subscription is successfully created.</p>
-                    <Link to="/dashboard">Click Here to go Dashboard</Link>
+                    <span className="circle--check"><FaRegCheckCircle /></span>
+                    <h2 className="text-center mb-1">Welcome to Prime Teams!</h2>
+                    <p className="text-center mb-4">Your payment has been processed successfully and your subscription is now active.</p>
+                    <Link className="btn btn-primary" to="/dashboard">Go toDashboard</Link>
                 </Container>
             </div>
         </div>
